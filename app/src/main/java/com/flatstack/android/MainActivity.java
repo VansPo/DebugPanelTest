9b76898829db7101942efb772ed5c8649029d380
Dmitriy Voronin <dsvoronin@gmail.com>
2014-10-09 15:24:48 +0400

    fragments mentions deleted

--- app/src/main/java/com/flatstack/android/MainActivity.java
+++ app/src/main/java/com/flatstack/android/MainActivity.java
@@ -1,20 +1,16 @@
 package com.flatstack.android;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.flatstack.android.common.Preferences;
 import com.flatstack.android.dagger.Dagger;
 import com.flatstack.android.dagger.Injector;
 import com.flatstack.android.dagger.modules.MainActivityScopeModule;
-import com.flatstack.android.fragments.MainFragment;
-import com.flatstack.android.fragments.PrefsFragment;
-import com.flatstack.android.common.Preferences;
 import com.github.mttkay.memento.Memento;
 import com.github.mttkay.memento.MementoCallbacks;
 import com.github.mttkay.memento.Retain;
@@ -37,19 +33,10 @@ public class MainActivity extends Activity implements MementoCallbacks, Injector
     super.attachBaseContext(new CalligraphyContextWrapper(newBase));
   }
 
-  @Override public void onAttachFragment(Fragment fragment) {
-    Memento.retain(this);
-    super.onAttachFragment(fragment);
-  }
-
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     Memento.retain(this);
     Dagger.inject(this);
-    FragmentManager fm = getFragmentManager();
-    if (fm.findFragmentById(android.R.id.content) == null) {
-      fm.beginTransaction().add(android.R.id.content, new MainFragment()).commit();
-    }
   }
 
   @Override public void onLaunch() {
@@ -64,14 +51,9 @@ public class MainActivity extends Activity implements MementoCallbacks, Injector
   @Override public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     switch (item.getItemId()) {
       case android.R.id.home:
-        getFragmentManager().popBackStack();
         return true;
 
       case R.id.action_settings:
-        getFragmentManager().beginTransaction()
-            .replace(android.R.id.content, new PrefsFragment())
-            .addToBackStack(null)
-            .commit();
         return true;
 
       default:
--- app/src/main/java/com/flatstack/android/dagger/Dagger.java
+++ app/src/main/java/com/flatstack/android/dagger/Dagger.java
@@ -1,6 +1,5 @@
 package com.flatstack.android.dagger;
 
-import android.app.Fragment;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.view.View;
@@ -21,13 +20,5 @@ public class Dagger {
 
   public static void inject(@NonNull View view) {
     getObjectGraph(view.getContext()).inject(view);
-  }
-
-  public static void inject(@NonNull Fragment fragment) {
-    getObjectGraph(fragment.getActivity()).inject(fragment);
-  }
-
-  public static void inject(@NonNull ScopedFragment fragment) {
-    getObjectGraph(fragment.getScopedContext()).inject(fragment);
   }
 }
